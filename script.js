574245d448757d44a26ca6bbf91f00c6d4122052
Guilherme Lunghini Teixeira <161391446+glunghini@users.noreply.github.com>
2024-04-24 22:18:56 -0300

    Página de login

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ function verificar() {
     }
 
 }
+
+
+var usuario = "1234"
+var senha = "1234"
+function logar(){
+    var login = document.getElementById("email").value;
+    var pass = document.getElementById("password").value;
+    if (login == usuario && pass == senha){
+        Swal.fire({
+            icon: "success",
+            title: "Parabéns",
+            text: "Você entrou na sua conta!",
+            timer:  1500
+        })
+        setTimeout(function() {
+            window.location.href = "homepage.html";
+        }, 3000);
+    }
+
+    else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Parece que você errou sua conta!",
+            timer: 1500
+          });
+    }
+}
